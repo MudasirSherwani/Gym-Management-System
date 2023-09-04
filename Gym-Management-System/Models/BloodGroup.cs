3164f3082cec2c77524b979a2dbacd0b4e157625
dell-laptop <mudasir9757@gmail.com>
2023-09-05 00:25:36 +0500

    Add Blood Group Model

---
+++ Gym-Management-System/Models/BloodGroup.cs
@@ -0,0 +1,18 @@
+﻿using Gym_Management_System.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection;
+
+namespace GymMGT.Models
+{
+    public class BloodGroup
+    {
+        [Key]
+        public int BloodGroupID { get; set; }
+        public string BloodGroupName { get; set; }
+        public virtual ICollection<GymTrainee> GymTrainees { get; set; }
+
+
+    }
+}
