a471a7aec1c8b37704b13cd76c2ac8de79328d72
dell-laptop <mudasir9757@gmail.com>
2023-09-06 00:26:01 +0500

    Add TrainingLevel Model

---
+++ Gym-Management-System/Models/TrainingLevel.cs
@@ -0,0 +1,14 @@
+﻿using Gym_Management_System.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace GymMGT.Models
+{
+    public class TrainingLevel
+    {
+        [Key]
+        public int TrainingLevelID { get; set; }
+        public string TrainingLevelName { get; set; }
+        public virtual ICollection<GymTrainee> GymTrainees { get; set; }
+    }
+}
