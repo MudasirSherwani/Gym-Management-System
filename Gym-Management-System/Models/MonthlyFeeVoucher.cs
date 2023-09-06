6aa9ce2d7b0833b7630e522169559dcf70024ad4
dell-laptop <mudasir9757@gmail.com>
2023-09-07 00:26:27 +0500

    Add MonthlyFeeVoucher Model

---
+++ Gym-Management-System/Models/MonthlyFeeVoucher.cs
@@ -0,0 +1,33 @@
+﻿using Gym_Management_System.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GymMGT.Models
+{
+    public class MonthlyFeeVoucher
+    {
+
+        [Key]
+        public int MonthlyFeeID { get; set; }
+
+        //[Timestamp]
+
+        [DataType(DataType.Date)]
+        public DateTime FeeDate { get; set; }=DateTime.Now;
+
+        public string Remarks { get; set; }
+
+        public string Status { get; set; }
+
+        [ForeignKey("GymTrainee")]
+        public int TraineeId { get; set; }
+        public GymTrainee GymTrainee { get; set; }
+
+
+      
+
+
+    }
+}
