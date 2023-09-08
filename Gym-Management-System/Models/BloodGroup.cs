e6e2fc7fbf990fdbec291b298f1d1bd9b007a4d1
dell-laptop <mudasir9757@gmail.com>
2023-09-08 12:11:51 +0500

    update model Blood Group

--- Gym-Management-System/Models/BloodGroup.cs
+++ Gym-Management-System/Models/BloodGroup.cs
@@ -1,5 +1,4 @@
-﻿using Gym_Management_System.Models;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Reflection;
