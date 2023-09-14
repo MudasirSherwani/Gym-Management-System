5bdde77fb3717b4f594fd3ec8877947119aa3d42
dell-laptop <mudasir9757@gmail.com>
2023-09-14 12:16:09 +0500

    Add TraineeDetailsViewModel

---
+++ Gym-Management-System/Models/TraineeDetailsViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Net;
+
+namespace GymMGT.Models
+{
+    public class TraineeDetailsViewModel
+    {
+        //public GymTrainee GymTrainee { get; set; }
+
+        public GymTrainee GymTraineeVM { get; set; }
+        public  BloodGroup BloodGroupVM { get; set; }
+
+        public TrainingLevel TrainingLevelVM { get; set; }
+
+
+        public MonthlyFeeVoucher MonthlyFeeVoucherVM { get; set; }
+
+        public string PageTitle { get; set; }
+        public string PageHeader { get; set; }
+    }
+}
