c149c53a722c1d73db1f7699874478326dec7134
dell-laptop <mudasir9757@gmail.com>
2023-09-14 12:15:34 +0500

    Update MonthlyFeeVoucher

--- Gym-Management-System/Models/MonthlyFeeVoucher.cs
+++ Gym-Management-System/Models/MonthlyFeeVoucher.cs
@@ -1,5 +1,4 @@
-﻿using Gym_Management_System.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
